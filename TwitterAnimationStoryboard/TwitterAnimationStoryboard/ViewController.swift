67392ab135eb184e6bd805d09384666ea47afdd0
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-01-09 11:47:25 +0530

    added like animation
    added like animation by playing images

--- TwitterAnimationStoryboard/TwitterAnimationStoryboard/ViewController.swift
+++ TwitterAnimationStoryboard/TwitterAnimationStoryboard/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.isUserInteractionEnabled = true
+        imgView.image = UIImage(named: "tile00")
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    var likeImages = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(imageView)
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(animateImage)))
+        imageView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        for i in 0...28{
+            likeImages.append(UIImage(named: "tile0\(i)")!)
+        }
     }
 
+    @objc func animateImage(){
+        imageView.animationImages = likeImages
+        imageView.animationDuration = 0.6
+        imageView.animationRepeatCount = 1
+        imageView.image = likeImages.last
+        imageView.startAnimating()
+    }
 
 }
 
