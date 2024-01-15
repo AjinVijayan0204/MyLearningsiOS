c54307f90b252654753e8a83e8b0f3fe099a0e90
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-01-15 16:35:16 +0530

    completed todo app

--- Storyboard/ToDoList/ToDoList/ViewController.swift
+++ Storyboard/ToDoList/ToDoList/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var items: [String] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        let itemObjects = UserDefaults.standard.object(forKey: "items")
+        if let tempItems = itemObjects as? [String]{
+            items = tempItems
+        }
+        tableView.reloadData()
+        print("came")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        var content = cell.defaultContentConfiguration()
+        content.text = items[indexPath.row] as String
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            items.remove(at: indexPath.row)
+            tableView.reloadData()
+            UserDefaults.standard.set(items, forKey: "items")
+        }
+    }
+    
 }
 
