0a295fb9bd96dd3392d28db4ad6f317e6c319811
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-01-15 16:38:26 +0530

    weather app created

---
+++ Storyboard/Whats The Weather/Whats The Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whats The Weather
+//
+//  Created by Ajin on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
