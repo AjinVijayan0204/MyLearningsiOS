075cfc1aa8e2238bdf77c9c18db5082fd7b3ff9e
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-01-21 19:44:32 +0530

    memorable places inital proj creation

---
+++ Storyboard/Memorable Places/Memorable Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Ajin on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
