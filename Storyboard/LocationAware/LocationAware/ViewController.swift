2082b6514f002da5eaf34fb31360890b0a6191a8
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-01-21 17:53:03 +0530

    added location
    added location and updated live location in mapview

--- Storyboard/LocationAware/LocationAware/ViewController.swift
+++ Storyboard/LocationAware/LocationAware/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var nearestAdressLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locManager.delegate = self
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        locManager.requestWhenInUseAuthorization()
+        locManager.startUpdatingLocation()
     }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        self.latitudeLabel.text = String(location.coordinate.latitude)
+        self.longitudeLabel.text = String(location.coordinate.longitude)
+        self.speedLabel.text = String(location.speed)
+        CLGeocoder().reverseGeocodeLocation(location) { placemark, error in
+            if let error = error{
+                print(error)
+            }else{
+                if let placemark = placemark?[0]{
+                    var address = ""
+                    if let subThoroughfare = placemark.subThoroughfare{
+                        address += subThoroughfare + " "
+                    }
+                    if let thoroughfare = placemark.thoroughfare{
+                        address += thoroughfare + " "
+                    }
+                    if let subLocality = placemark.subLocality{
+                        address += subLocality + " "
+                    }
+                    if let postalCode = placemark.postalCode{
+                        address += postalCode + " "
+                    }
+                    if let country = placemark.country{
+                        address += country + " "
+                    }
+                    self.nearestAdressLabel.text = address
+                    
+                    let (lanDelta, longDelta): (CLLocationDegrees, CLLocationDegrees) = (0.05, 0.05)
+                    let span = MKCoordinateSpan(latitudeDelta: lanDelta, longitudeDelta: longDelta)
+                    let coordinates = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                    let annotation = MKPointAnnotation()
+                    annotation.title = "Me"
+                    annotation.coordinate = coordinates
+                    let region = MKCoordinateRegion(center: coordinates, span: span)
+                    self.mapView.setRegion(region, animated: true)
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+}
