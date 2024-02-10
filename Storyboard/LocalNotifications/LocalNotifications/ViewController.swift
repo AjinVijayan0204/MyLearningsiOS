e4622d2d4f14929f2394a44f6ba8d41720a41782
AjinVijayan0204 <108613536+AjinVijayan0204@users.noreply.github.com>
2024-02-10 21:13:15 +0530

    created localnotifications app

---
+++ Storyboard/LocalNotifications/LocalNotifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by Ajin on 10/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
